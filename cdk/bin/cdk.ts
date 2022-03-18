3a2ae8fd83052128c121cd7d6683660f08f86256
Florian Clanet <florianclanet@gmail.com>
2022-03-18 23:41:56 +0100

    feat: cdk stack + dynamodb table

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,20 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const regionsToDeploy = ['us-east-1', 'us-east-2'];
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+regionsToDeploy.forEach((regionCode) => {
+  const stack = new CdkStack(app, `WebSocketStack-${regionCode}`, {
+    env: { region: regionCode },
+    regionCodeToReplicate: regionsToDeploy.filter((replicationRegion) => replicationRegion !== regionCode)
+  });
+  Tags.of(stack).add('project', 'multi-region-websocket');
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,24 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new Table(this, 'WebsocketConnections', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+      tableName: 'WebsocketConnections',
+      partitionKey: {
+        name: 'chatId',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'connectionId',
+        type: AttributeType.STRING
+      },
+    });
+    
   }
 }
